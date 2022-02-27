bb11bd6216d126c2ebb7a84e5f2304f0f9377c14
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2022-02-28 02:02:35 +0530

    Assignment1 -> Q1 implementation

---
+++ Assignment1/code/utilities/PCA.py
@@ -0,0 +1,77 @@
+import imp
+import numpy as np
+import math
+
+
+def process(data):
+    """
+    Find out the variance matrix for the given data
+
+        Parameters
+        ----------
+        data : 
+              matrix data 
+        Returns
+        -------        
+    eigen_values and eigen_vectors
+
+        """
+    covariance = data.T @ data  # Matric multlication
+    eigen_values, eigen_vectors = np.linalg.eigh(covariance)
+
+    return eigen_values, eigen_vectors
+
+
+def kernalPCA(data, function_pointer, d_value, no_ocmponents):
+    """
+    kernal PCA
+
+    Parameters
+    ----------
+    data 
+    function_pointer
+    d_value
+    no_ocmponents
+    
+    Returns
+    -------        
+    eigen_values and eigen_vectors
+
+        """
+    nk = []
+    for i in range(len(data)):
+        n = []
+        for j in range(len(data)):
+            n.append(function_pointer(data[i, :], data[j, :], d_value).item())
+        nk.append(n)
+
+    # nk= K @ K.T
+    d = np.matrix(nk)
+
+    eigen_values, eigen_vectors = np.linalg.eigh(d)
+    eigen_values = eigen_values[-1:(-1+-1*no_ocmponents):-1]
+    eigen_vectors = eigen_vectors[:, -1:(-1)+no_ocmponents:-1]
+
+    # this will put the eigenvector in row
+    eigen_vectors = [(eigen_vectors[:, i]/(len(data)*eigen_values[i]) ** (1/2)).getA1() for i in range(no_ocmponents)]
+    eigen_vectors = np.matrix(eigen_vectors)
+
+    projected_data = d  @ eigen_vectors.T
+
+    return eigen_values, projected_data
+
+ 
+def polynominal_function(x, y, v):
+    """
+    Implementation for this (1 + x^Ty)^d for d = {2,3} function
+
+        """
+    return (1 + x @ y.T)**v
+
+
+def gauusian_function(x, y, p):
+    """
+    Implementation for this e^(-(x-y)(x-y)^T / 2*(sigam)^2) for d = {2,3} function
+
+        """
+    return np.matrix(math.exp(((y-x) @ (x-y).T)/(2*p*p)))
---
+++ Assignment1/code/utilities/common.py
@@ -0,0 +1,92 @@
+# ***************************************************************************************************************************
+# File      : Common.py 
+# Purpose   : Provides access to common utility functionalities ( read file)
+# Author    : Hanumantappa Budihal
+#
+# Date      : 23-02-2022 
+# Bugs      : NA
+# Change Log:
+# ****************************************************************************************************************************/
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data(file_path):
+    """
+	Read the data from the file using pandas framework and
+    return the data.
+
+	Parameters
+	----------
+	file_path : 
+              path for file 
+	Returns
+	-------        
+    return the data from the file
+     
+	"""
+    data = pd.read_csv(file_path, header=None)
+    data = np.matrix(data)
+
+    return data
+
+
+def center_data(data):
+    """
+	Centred the data by subtracing the mean from each data point.
+
+	Parameters
+	----------
+	data : 
+          input data 
+	Returns
+	-------        
+    centred data
+     
+	"""
+    data = data-data.sum(axis=0)/len(data)
+
+    return data
+
+
+def plot_data(original_data, projected_data, eigenvalues, colour):
+    """
+	Plot the given set of the data sets
+
+	Parameters
+	----------
+	data : 
+          original_data : original data set
+          projected_data : projected data set using pca
+          eigenvalues : eigen values
+          colour : data points color
+	Returns
+	-------        
+    None :
+
+    Plot the all 3 type graph from given data     
+	"""
+
+    # original data
+    plt.figure(figsize=(18, 5))
+    
+    plt.subplot(131)
+    plt.title("original data")
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.plot(original_data[:, 0], original_data[:, 1], colour)
+
+    # projected data
+    plt.subplot(132)
+    plt.title("projected data")
+    plt.xlabel("x-axis/first major eigen vector")
+    plt.ylabel("y-axis/second major eigen vector")
+    plt.plot(projected_data[:, 0], projected_data[:, 1], colour)
+
+    # eigenvlaues plot
+    plt.subplot(133)
+    plt.title("eigenvlaues plot")
+    plt.xlabel("x-axis/no of eigen values")
+    plt.ylabel("y-axis/eigen vlaues")
+    plt.plot([i for i in range(1, len(eigenvalues)+1)], eigenvalues, colour)
