fc6d5239b883d1cadd8663165f76860cdf1cb65f
Hanumantappa Budihal <33819543+HanumantappaBudihal@users.noreply.github.com>
2022-04-06 12:00:01 +0530

    Question 1a and 1b

--- Assignment2/code/Q1a.py
+++ Assignment2/code/Q1a.py
@@ -1,62 +1,108 @@
-# import numpy as np
-# import csv
-# import matplotlib.pyplot as plt
-# x = []
-
-# with open('A2Q1Data.csv','r') as csvfile:
-#     plots = csv.reader(csvfile, delimiter = ',')
-#     for row in plots:
-#         x.append(int(row[0]))
-
-# X = np.array(x)
-# X = X.reshape((1000,1))
-# N = 1000
-# # plt.hist(X, bins = 100)
-# # plt.title('Original data')
-# # plt.xlabel('X (data points)')
-# # plt.ylabel('PMF(X)')
-# # plt.show()
-
-# No_Iterations = 30
-# K  = 4
-
-# np.random.seed(0)
-# Lambda = np.zeros((N,K))
-# denom=np.array(0)
-# logLL = np.zeros((30,1))
-# for I in range(0, 100):
-#     np.random.seed(0) 
-#     P = np.random.default_rng().uniform(0,1,K)
-#     np.random.seed(0) 
-#     Pi = np.random.default_rng().uniform(0,1,K)
-#     Pi /= Pi.sum()
-#     for T in range(No_Iterations):
-#         Temp_P = np.zeros((K,1))
-#         for i in range(N):
-#             denom = 0
-#             for l in range(K):
-#                 denom += (pow(P[l],X[i]) * pow(1-P[l], 1-X[i])) * Pi[l]
-#             for k in range(K):
-#                 Lambda[i][k] = ((pow(P[k],X[i]) * pow(1-P[k], 1-X[i])) * Pi[k]) / denom
-#                 logLL[T] +=  Lambda[i][k] * (np.log(Pi[k]) + np.log(pow(P[l],X[i]) * pow(1-P[l], 1-X[i])) - np.log(Lambda[i][k]))
-#                 Temp_P[k] += Lambda[i][k] * X[i]
-#         Pi = Lambda.sum(axis = 0)
-#         for k in range(K):
-#             P[k] = Temp_P[k]/Pi[k]
-#         Pi = Pi/N
-# logLL = logLL / 100
-# for i in range(30):
-#     plt.scatter(i,logLL[i])
-# plt.title('BMM')
-# plt.xlabel('No of Iterations')
-# plt.ylabel('Log likelihood')
-# plt.show()
-
-
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import random 
 
-data=pd.read_csv("../dataset/A2Q1.csv")
+# calculate exponential pdf
+def expo(x,s):
+    return (s * np.exp(-(s*x)))
 
-plt.hist(data)
+# expectation - calculate lambda to maximize log likelihood
+def lMML(x,s,p):
+    n = x.shape[0]
+    k = len(s)
+    l = np.zeros((n,k), dtype= float)
+    for i in range(n):
+        for j in range(k):
+            l[i][j] = p[j]*expo(x[i],s[j])
+            
+        l[i] /= np.sum(l[i]) 
+
+    return l
+
+# maximization - calculate parameters to maximize log likelihood
+def pMML(x,l):
+    s = np.zeros(l.shape[1])
+    p = np.zeros(l.shape[1])
+    for k in range(l.shape[1]):
+        total = np.sum(l[:,k])
+        for i in range(l.shape[0]):
+            s[k] += (l[i][k]*(x[i]))
+        s[k] = total / s[k]
+        p[k] = total / l.shape[0]
+
+    return s,p
+
+# calculate log likelihood
+def calcLL(x,s,p):
+    logl = 0
+    for i in range(x.shape[0]):
+        inside = 0
+        for k in range(len(s)):
+            inside += p[k]*expo(x[i],s[k])
+        logl += np.log(inside)    
+    return logl
+
+# initialize parameters
+def init_params(data, k):
+    # random initialization of parameters
+    s = np.random.rand(k)
+    p = np.random.rand(k)
+    p = p/p.sum()
+    return s,p
+
+# load the dataset
+file = open("../dataset/A2Q1.csv")
+data = np.loadtxt(file, delimiter=",")
+
+# no of datapoints
+n = data.shape[0]
+# no of distribution in mixture
+k = 4
+# no of random initializations
+no_of_iterations = 30
+# tolerence value
+epsilon = 0.0001
+# to store log likelihoods of all initializations
+allLL = []
+
+# EM Algorithm
+for itr in range(no_of_iterations):
+    print(itr)
+    s,p = init_params(data,k)
+
+    # to store log likelihoods
+    ll = []
+    
+    convergence = True
+    while(convergence):
+
+        # estimation step
+        l = lMML(data,s,p)
+        
+        # maximization step
+        s,p = pMML(data,l)
+
+        # calculate log likelihood
+        ll.append(calcLL(data,s,p))
+
+        # check convergence
+        if len(ll)>1 and np.abs(np.abs(ll[-1])-np.abs(ll[-2])) < epsilon:
+            convergence = False
+    
+    allLL.append(ll)
+
+# to make the length of all rows same
+mlen = len(max(allLL, key=len))
+for i in range(len(allLL)):
+    if len(allLL[i]) < mlen:
+        allLL[i] = allLL[i] + (mlen-len(allLL[i]))*[allLL[i][-1]]
+
+# calculate average log likelihood over random initializations
+avgLL = np.mean(allLL, axis=0)
+
+# plot the average log likelihood vs iterations graph
+plt.plot(avgLL)
+plt.title('log-likelihood (averaged over 100 random initializations) as a function of iterations')
+plt.xlabel('No. of iterations')
+plt.ylabel('log likelihood')
 plt.show()
--- Assignment2/code/Q1b.py
+++ Assignment2/code/Q1b.py
@@ -1,69 +1,131 @@
 import numpy as np
-import csv
 import matplotlib.pyplot as plt
+import random
 
-from torch import double
+# calculate gaussian pdf
 
-x = []
 
-with open('../dataset/A2Q1.csv', 'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append((np.float64)(row[0]))
+def gauss(x, m, s):
+    return (np.exp(-((x-m)**2)/(2*s)) / np.sqrt(2*np.pi*s))
 
-X = np.array(x)
-X = X.reshape((1000, 1))
-N = 1000
+# expectation - calculate lambda to maximize log likelihood
 
-K = 4
 
-np.random.seed(0)
-Lambda = np.zeros((N, K))
-sig = np.zeros((4, 1))
+def lMML(x, m, s, p):
+    n = x.shape[0]
+    k = len(m)
+    l = np.zeros((n, k), dtype=float)
+    for i in range(n):
+        for j in range(k):
+            l[i][j] = p[j]*gauss(x[i], m[j], s[j])
 
-logLL = np.zeros((30, 1))
-for I in range(0, 100):
-    mu = np.random.default_rng().uniform(0, 1, K)
-    sig = [0.2, 0.2, 0.2, 0.2]
-    Pi = np.random.default_rng().uniform(0, 1, K)
-    Pi /= Pi.sum()
-    print(Pi)
+        l[i] /= np.sum(l[i])
 
-    for T in range(30):
-        Temp_P = np.zeros((K, 1))
-        for i in range(N):
-            denom = 0
-            for l in range(K):
-                sigma = 2 * sig[l]
-                a = -((X[i] - mu[l])*(X[i] - mu[l]))
+    return l
 
-                denom += np.exp((a)/sigma) * Pi[l]
-            for k in range(K):
+# maximization - calculate parameters to maximize log likelihood
 
-                sigma = 2 * sig[k]
-                a = -(X[i] - mu[k])*(X[i] - mu[k])
 
-                Lambda[i][k] = (np.exp(a/sigma) * Pi[k]) / denom
-                b = ((X[i] - mu[k])*(X[i] - mu[k]))
-                c=Lambda[i][k] * \
-                    (np.log(Pi[k]) - (b/sigma) - np.log(Lambda[i][k]))
+def pMML(x, l):
 
-                if(not np.isnan(c)):
-                    logLL[T] += c
+    m = np.zeros(l.shape[1])
+    s = np.zeros(l.shape[1])
+    p = np.zeros(l.shape[1])
 
-            Pi = Lambda.sum(axis=0)
-            for k in range(K):
-                Temp_P[k] += Lambda[i][k] * X[i]
-                
-        for z in range(K):
-            mu[z] = Temp_P[z] / Pi[z]
-        Pi = Pi/N
+    for k in range(l.shape[1]):
+        total = np.sum(l[:, k])
 
-logLL = logLL / 100
+        # calculate means
+        for i in range(l.shape[0]):
+            m[k] += (l[i][k]*x[i])
+        m[k] /= total
 
-plt.plot(logLL)
-plt.title('GMM')
-plt.xlabel('No of Iterations')
-plt.ylabel('Log likelihood')
-plt.show()
-plt.savefig("Test.png")
+        # calculate variances
+        for i in range(l.shape[0]):
+            s[k] += (l[i][k]*((x[i]-m[k])**2))
+        s[k] /= total
+
+        # calculate probabilities of each distribution in mixture
+        p[k] = total / l.shape[0]
+
+    return m, s, p
+
+# calculate log likelihood
+
+
+def calcLL(x, m, s, p):
+    logl = 0
+    for i in range(x.shape[0]):
+        inside = 0
+        for k in range(len(m)):
+            inside += p[k]*gauss(x[i], m[k], s[k])
+        logl += np.log(inside)
+    return logl
+
+# initialize parameters
+
+
+def init_params(data, k):
+    # random initialization of parameters
+    m = random.sample(list(data), k)
+    s = np.random.rand(k)*(data.shape[0] / k)
+    p = np.random.rand(k)
+    p = p/p.sum()
+    return m, s, p
+
+
+# load the dataset
+file = open("../dataset/A2Q1.csv")
+data = np.loadtxt(file, delimiter=",")
+
+# no of datapoints
+n = data.shape[0]
+# no of distribution in mixture
+k = 4
+# no of random initializations
+no_of_iterations = 100
+# tolerence value
+epsilon = 0.0001
+# to store log likelihoods of all initializations
+allLL = []
+
+# EM Algorithm
+for itr in range(no_of_iterations):
+
+    m, s, p = init_params(data, k)
+
+    # to store log likelihoods
+    ll = []
+
+    convergence = True
+    while(convergence):
+
+        # estimation step
+        l = lMML(data, m, s, p)
+
+        # maximization step
+        m, s, p = pMML(data, l)
+
+        # calculate log likelihood
+        ll.append(calcLL(data, m, s, p))
+
+        # check convergence
+        if len(ll) > 1 and np.abs(np.abs(ll[-1])-np.abs(ll[-2])) < epsilon:
+            convergence = False
+
+    allLL.append(ll)
+
+# to make the length of all rows same
+mlen = len(max(allLL, key=len))
+for i in range(len(allLL)):
+    if len(allLL[i]) < mlen:
+        allLL[i] = allLL[i] + (mlen-len(allLL[i]))*[allLL[i][-1]]
+
+# calculate average log likelihood over random initializations
+avgLL = np.mean(allLL, axis=0)
+
+# plot the average log likelihood vs iterations graph
+plt.plot(avgLL)
+plt.xlabel('No. of iterations')
+plt.ylabel('log likelihood')
+plt.savefig("../dataset/Q1b.png", dpi=300)
